ebd4c1d0eb73e034690b3d11789eeee5114db9b5
DanielDeJesusAraujo <danielaraljo115@gmail.com>
2023-08-18 19:45:23 -0300

    feat: filters by country and categorys

---
+++ extewsroject/src/app/components/header/index.tsx
@@ -0,0 +1,57 @@
+'use client'
+import React from 'react'
+import styles from './page.module.css'
+
+function Header({handleNews}: () => void) {
+  const newsCategories = [
+    "Politic",
+    "Technology",
+    "Sports",
+    "Entertainment",
+    "Health",
+    "Science",
+    "Business",
+    "General",
+  ];
+
+  const countrys = [
+    "us",
+    "br",
+    "ru",
+    "gb",
+    "es",
+    "ca",
+    "in",
+    "au",
+    "de",
+    "fr",
+    "it",
+    "jp",
+    "cn",
+    "ar",
+    "tr",
+    "nl",
+    "mx",
+  ]
+
+  return (
+    <header className={styles.header}>
+      <h1>NextNews</h1>
+      <section>
+        <select name="country" onChange={handleNews}>
+          {countrys.map((country) => (
+            <option key={country} value={country}>{country}</option>
+          ))}
+        </select>
+        <select name="category" onChange={handleNews}>
+          {newsCategories.map((category) => (
+            <option key={category} value={category}>{category}</option>
+          ))}
+        </select>
+      </section>
+      <nav></nav>
+    </header>
+  )
+}
+
+export default Header
--- extewsroject/src/app/components/news/index.tsx
+++ extewsroject/src/app/components/news/index.tsx
@@ -1,19 +1,61 @@
 /* eslint-disable @next/next/no-img-element */
-import IApiResponse from './interfaces/Inews'
+'use client'
+import styles from './page.module.css'
+import IApiResponse from '../../interfaces/Inews'
+import Header from '../header';
+import { useEffect, useState } from 'react';
 
-const getNews = async (): Promise<IApiResponse> => {
-  const news = await fetch(`https://newsapi.org/v2/everything?q=tesla&from=2023-07-18&sortBy=publishedAt&apiKey=${process.env.NEXT_PUBLIC_NEWS_API_KEY || '994d196c82444a109712477579373004'}`)
+const newsCategories = [
+  "Politics",
+  "Technology",
+  "Sports",
+  "Entertainment",
+  "Health",
+  "Science",
+  "Business",
+  "General",
+];
+
+
+const getNews = async ({country, category}: {country?: string, category?: string}): Promise<IApiResponse> => {
+  const news = await fetch(`https://newsapi.org/v2/top-headlines?country=${country || 'us'}&category=${category || newsCategories[1]}&apiKey=${process.env.NEXT_PUBLIC_NEWS_API_KEY || '994d196c82444a109712477579373004'}`)
   const data = await news.json()
   return data
 }
 
-export default async function Home() {
-  const news = await getNews()
-  console.log(`>>> Results: ${news.articles.length} | Total Results: ${news.totalResults}***********************`);
+
+
+export default function News() {
+  const [filters, setfilters] = useState({
+    country: 'us',
+    category: newsCategories[1],
+  })
+  const [news, setnews] = useState<IApiResponse>()
+
+  useEffect(() => {
+    const get = async() => {
+      const res = await getNews(filters)
+      setnews(res)
+    }
+    get()
+  }, [filters])
+
+  useEffect(() => {
+    console.log(filters);
+    
+  }, [filters])
+  
+
+  const handleNews = ({target: {value, name}}: {target: {value: string, name: string}}) => {
+    setfilters({...filters, [name]: value})
+  }
+
+
   return (
-    <main>
-      {news.articles.map((article) => (
-        <article key={article.url}>
+    <main className={styles.news}>
+      <Header handleNews={handleNews}/>
+      {news && news.articles.map((article) => (
+        <article className={styles.article} key={article.url}>
           <h2>{article.title}</h2>
           <img src={article.urlToImage} alt={article.title} width={500} height={300} />
           <p>{article.description}</p>
