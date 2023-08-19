9e37bd1a2328fc3a9a369815af6db546ccce5a7c
DanielDeJesusAraujo <danielaraljo115@gmail.com>
2023-08-19 10:30:49 -0300

    fix: undefined return error

--- extewsroject/src/app/components/news/index.tsx
+++ extewsroject/src/app/components/news/index.tsx
@@ -5,9 +5,8 @@ import IApiResponse from '../../interfaces/Inews'
 import Header from '../header';
 import { useEffect, useState } from 'react';
 
-
 const getNews = async ({country, category}: {country?: string, category?: string}): Promise<IApiResponse> => {
-  const news = await fetch(`https://newsapi.org/v2/top-headlines?country=${country || 'us'}&category=${category || 'General'}&apiKey=${process.env.NEXT_PUBLIC_NEWS_API_KEY}`)
+  const news = await fetch(`https://newsapi.org/v2/top-headlines?country=${country || 'us'}&category=${category || 'General'}&apiKey=${process.env.NEXT_PUBLIC_NEWS_API_KEY || '994d196c82444a109712477579373004'}`)
   const data = await news.json()
   return data
 }
@@ -29,34 +28,28 @@ export default function News() {
     get()
   }, [filters])
 
-  useEffect(() => {
-    console.log(filters);
-    
-  }, [filters])
-  
-
   const handleNews = (
     {target: {value, name}}: {target: {value: string, name: string}}
     ) => {
     setfilters({...filters, [name]: value})
   }
 
-
-  if (news?.totalResults) {
+  if (news && news.totalResults) {
     return (
       <main className={styles.news}>
         <Header handleNews={handleNews}/>
-        {news.articles.map((article) => (
+        {/* {news.articles.map((article) => (
           <article className={styles.article} key={article.url}>
             <h2>{article.title}</h2>
             <img src={article.urlToImage} alt={article.title} width={500} height={300} />
             <p>{article.description}</p>
           </article>
-        ))}
+        ))} */}
       </main>
     )
   }
+
   return (
-    <main></main>
+    <main>loading...</main>
   )
 }
