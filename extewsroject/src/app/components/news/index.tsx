2054cdbaef5e1aacab7996b39c311d79baa07263
DanielDeJesusAraujo <danielaraljo115@gmail.com>
2023-08-19 11:02:25 -0300

    fix: undefined return error

--- extewsroject/src/app/components/news/index.tsx
+++ extewsroject/src/app/components/news/index.tsx
@@ -5,7 +5,7 @@ import IApiResponse from '../../interfaces/Inews'
 import Header from '../header';
 import { useEffect, useState } from 'react';
 
-const getNews = async ({country, category}: {country?: string, category?: string}): Promise<IApiResponse> => {
+const getNews = async ({ country, category }: { country?: string, category?: string }): Promise<IApiResponse> => {
   const news = await fetch(`https://newsapi.org/v2/top-headlines?country=${country || 'us'}&category=${category || 'General'}&apiKey=${process.env.NEXT_PUBLIC_NEWS_API_KEY || '994d196c82444a109712477579373004'}`)
   const data = await news.json()
   return data
@@ -21,7 +21,7 @@ export default function News() {
   const [news, setnews] = useState<IApiResponse>()
 
   useEffect(() => {
-    const get = async() => {
+    const get = async () => {
       const res = await getNews(filters)
       setnews(res)
     }
@@ -29,23 +29,21 @@ export default function News() {
   }, [filters])
 
   const handleNews = (
-    {target: {value, name}}: {target: {value: string, name: string}}
-    ) => {
-    setfilters({...filters, [name]: value})
+    { target: { value, name } }: { target: { value: string, name: string } }
+  ) => {
+    setfilters({ ...filters, [name]: value })
   }
 
-  if (news && news.totalResults) {
-    return (
-      <main className={styles.news}>
-        <Header handleNews={handleNews}/>
-        {news.articles.map((article) => (
-          <article className={styles.article} key={article.url}>
-            <h2>{article.title}</h2>
-            <img src={article.urlToImage} alt={article.title} width={500} height={300} />
-            <p>{article.description}</p>
-          </article>
-        ))}
-      </main>
-    )
-  }
+  return (
+    <main className={styles.news}>
+      <Header handleNews={handleNews} />
+      {news ? (news.articles.map((article) => (
+        <article className={styles.article} key={article.url}>
+          <h2>{article.title}</h2>
+          <img src={article.urlToImage} alt={article.title} width={500} height={300} />
+          <p>{article.description}</p>
+        </article>
+      ))): <p>Loading...</p>}
+    </main>
+  )
 }
